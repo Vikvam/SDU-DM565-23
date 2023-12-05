6296146c5c374d14e0722f3fac0eda0ebd50176f
Mateusz Poczta <mapoc23@student.sdu.dk>
2023-12-05 14:24:28 +0100

    added conf properties

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from props_reader import PropertiesReader
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+PROPERTIES_FILE = "conf.properties"
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    props_reader = PropertiesReader()
+    props = props_reader.load_properties(PROPERTIES_FILE)
+    print(props)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
