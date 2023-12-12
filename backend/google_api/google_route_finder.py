5ca55c00cab8bce5cc5af88ea71e55add2beea3f
Mateusz Poczta <mapoc23@student.sdu.dk>
2023-12-12 12:38:17 +0100

    added Google Maps API cleaning

--- backend/google_api/google_route_finder.py
+++ backend/google_api/google_route_finder.py
@@ -37,12 +37,57 @@ class GoogleRouteFinder:
         except requests.exceptions.HTTPError as err:
             raise SystemExit(err)
 
-        route = ResponseBody(**result.json())
-        print(route)
 
-        print(result.json())
+        result = GoogleRouteFinder._clean_response_body(result.json())
+        print(result)
+
+        # route = ResponseBody(**result.json())
+        # print(route)
+
+        # print(result.json())
 
     def send_request(self):
         result = requests.post(self.GOOGLE_ROUTES_URL, json=self.request_body, headers=self.request_headers)
         result.raise_for_status()
         return result
+
+    @staticmethod
+    def _clean_response_body(response_body):
+        response_body = GoogleRouteFinder._clean_data(response_body)
+        response_body = GoogleRouteFinder.remove_response_body_nesting(response_body)
+        return response_body
+
+    @staticmethod
+    def _clean_data(data):
+        new_dict = {}
+
+        if data:
+            for key, value in data.items():
+                if isinstance(value, dict):
+                    value = GoogleRouteFinder._clean_data(value)
+                elif isinstance(value, list):
+                    new_list = []
+
+                    for item in value:
+                        item = GoogleRouteFinder._clean_data(item)
+                        if item is not None:
+                            new_list.append(item)
+
+                    value = new_list
+
+                if value not in ("", None, {}, []):
+                    new_dict[key] = value
+
+            if new_dict == {}:
+                return None
+            else:
+                return new_dict
+
+        return None
+
+    @staticmethod
+    def remove_response_body_nesting(data):
+        for route in data['routes']:
+            route['legs'] = route['legs'][0]['steps']
+
+        return data
