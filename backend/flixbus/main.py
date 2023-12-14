4b71ff1c2094368c1b310b67f058a09d88ad93cc
Vikvam <vikvam@disroot.org>
2023-12-14 20:05:27 +0100

    DB mostly done

--- backend/flixbus/main.py
+++ backend/flixbus/main.py
@@ -1,22 +1,22 @@
 import os
+import sys
 from time import sleep
 from dotenv import load_dotenv
 import scrapy
 from scrapy import Selector
 from scrapy.crawler import CrawlerProcess
+from selenium.common import TimeoutException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from abc import ABC
 from dataclasses import dataclass
 from datetime import date
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-from selenium_middleware import SeleniumRequest
-
 
 load_dotenv()
 
+sys.path.append("..")
+from selenium_middleware import SeleniumRequest
 
 @dataclass
 class FlixbusRequest:
@@ -29,9 +29,10 @@ class FlixbusSpider(scrapy.Spider, ABC):
     name: str = "Flixbus shop spider"
     _BASE_URL = f"https://global.flixbus.com"
 
-    def __init__(self, request: FlixbusRequest, **kwargs):
+    def __init__(self, request: FlixbusRequest, timeout=5, **kwargs):
         super().__init__(**kwargs)
         self.request = request
+        self.timeout = timeout
 
     def start_requests(self) -> [SeleniumRequest]:
         yield SeleniumRequest(
@@ -41,45 +42,47 @@ class FlixbusSpider(scrapy.Spider, ABC):
             wait_until=self.page_loaded
         )
 
-    def selenium_accept_cookies(self, driver):
-        WebDriverWait(driver, 5).until(lambda driver: driver.find_element(By.ID, "usercentrics-root"))
-        shadow_root = driver.execute_script('return arguments[0].shadowRoot', driver.find_element(By.ID, "usercentrics-root"))
-        WebDriverWait(driver, 5).until(lambda driver: shadow_root.find_element(By.CSS_SELECTOR, "button.bqSTzv"))
-        driver.execute_script('return arguments[0].click()', shadow_root.find_element(By.CSS_SELECTOR, "button.bqSTzv"))
-
-    def selenium_input_search(self, driver):
-        input_from = driver.find_element(By.CSS_SELECTOR, "#searchInput-from")
-        input_from.clear()
-        input_from.send_keys(self.request.origin_place_name)
-        sleep(.5)
-        input_from.send_keys([Keys.DOWN, Keys.ENTER])
-
-        input_to = driver.find_element(By.CSS_SELECTOR, "#searchInput-to")
-        input_to.clear()
-        input_to.send_keys(self.request.destination_place_name)
-        sleep(.5)
-        input_to.send_keys([Keys.DOWN, Keys.ENTER])
-
-        search_button = driver.find_element(By.CSS_SELECTOR, "div[data-e2e='search-button'] > button")
-        search_button.click()
-
-    def selenium_url_search(self, driver):
-        url = driver.current_url.split("&rideDate")[0]
-        url += f"&rideDate={self.request.date.strftime('%d.%m.%Y')}&adult=1&_locale=en&features%5Bfeature.enable_distribusion%5D=1&features%5Bfeature.train_cities_only%5D=0&features%5Bfeature.auto_update_disabled%5D=0&features%5Bfeature.webc_search_station_suggestions_enabled%5D=0&features%5Bfeature.darken_page%5D="
-        print(url)
-        driver.get(url)
-
     def selenium_search(self, driver):
-        self.selenium_accept_cookies(driver)
-        self.selenium_input_search(driver)
-        self.selenium_url_search(driver)
+        def selenium_accept_cookies(driver):
+            def get_shadow_root_parent(driver): return driver.find_element(By.ID, "usercentrics-root")
+            WebDriverWait(driver, self.timeout).until(get_shadow_root_parent)
+            shadow_root = driver.execute_script('return arguments[0].shadowRoot', get_shadow_root_parent(driver))
+            def get_cookie_button(_): return shadow_root.find_element(By.CSS_SELECTOR, "button[data-testid='uc-deny-all-button']")
+            WebDriverWait(driver, self.timeout).until(get_cookie_button)
+            driver.execute_script('return arguments[0].click()', get_cookie_button(driver))
+
+        def selenium_input_search(driver):
+            input_from = driver.find_element(By.CSS_SELECTOR, "#searchInput-from")
+            input_from.clear()
+            input_from.send_keys(self.request.origin_place_name)
+            sleep(.5)
+            input_from.send_keys([Keys.DOWN, Keys.ENTER])
+
+            input_to = driver.find_element(By.CSS_SELECTOR, "#searchInput-to")
+            input_to.clear()
+            input_to.send_keys(self.request.destination_place_name)
+            sleep(.5)
+            input_to.send_keys([Keys.DOWN, Keys.ENTER])
+
+            search_button = driver.find_element(By.CSS_SELECTOR, "div[data-e2e='search-button'] > button")
+            search_button.click()
+
+        def selenium_url_search(driver):
+            url = driver.current_url
+            print(url)
+            url += url.split("&rideDate")[0] + f"&rideDate={self.request.date.strftime('%d.%m.%Y')}&adult=1&_locale=en&features%5Bfeature.enable_distribusion%5D=1&features%5Bfeature.train_cities_only%5D=0&features%5Bfeature.auto_update_disabled%5D=0&features%5Bfeature.webc_search_station_suggestions_enabled%5D=0&features%5Bfeature.darken_page%5D="
+            driver.get(url)
+
+        selenium_accept_cookies(driver)
+        selenium_input_search(driver)
+        selenium_url_search(driver)
 
     def page_loaded(self, driver):
         return driver.find_element(By.CSS_SELECTOR, "ul.ResultsList__resultsList___eGsLK")
 
     def parse(self, response: scrapy.http.Response, **kwargs):
         driver = response.request.meta["driver"]
-        WebDriverWait(driver, response.request.wait_timeout).until(self.page_loaded)
+        # WebDriverWait(driver, response.request.wait_timeout).until(self.page_loaded)
 
         def parse_route(selector):
             price_selector = selector.xpath(".//span[contains(@class, 'SearchResult__price___QpySa')]")
@@ -102,11 +105,10 @@ if __name__ == "__main__":
         },
         "SELENIUM_DRIVER_NAME": "firefox",
         "SELENIUM_DRIVER_EXECUTABLE_PATH": os.getenv("FIREFOX_PATH_DRIVER"),
-        "SELENIUM_DRIVER_ARGUMENTS": [], # ["--headless"],
+        "SELENIUM_DRIVER_ARGUMENTS": ["--headless"],
         "LOG_LEVEL": "WARNING",
     })
     # process.crawl(FlixbusSpider, request=FlixbusRequest("Berlin", "Copenhagen", date(2023, 12, 31)))
-    # print("---------------------")
     process.crawl(FlixbusSpider, request=FlixbusRequest("Praha", "Mnichov", date(2024, 1, 31)))
     process.start()
 
